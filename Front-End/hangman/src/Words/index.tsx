92973e593d974c53d03ba83712f6a254a2dbdd59
Gabriel Souza <gabrielmaestasouza@gmail.com>
2024-09-17 11:13:08 -0300

    Improves clue filter

--- Front-End/hangman/src/Words/index.tsx
+++ Front-End/hangman/src/Words/index.tsx
@@ -2,8 +2,7 @@ export async function getWordAndClue() {
     try {
       let wordData;
       let word = '';
-      
-    
+
       do {
         const randomWordResponse = await fetch('https://api.dicionario-aberto.net/random');
         if (!randomWordResponse.ok) {
@@ -11,12 +10,12 @@ export async function getWordAndClue() {
         }
         
         wordData = await randomWordResponse.json();
-        word = wordData.word.toUpperCase(); 
+        word = wordData.word.toUpperCase();
       } while (word.includes('-')); 
   
       const wordForPrefix = word.slice(0, -1).toLowerCase(); 
   
-     
+  
       const prefixResponse = await fetch(`https://api.dicionario-aberto.net/prefix/${wordForPrefix}`);
       if (!prefixResponse.ok) {
         throw new Error('Erro ao buscar a dica da palavra.');
@@ -24,20 +23,21 @@ export async function getWordAndClue() {
       
       const prefixData = await prefixResponse.json();
   
-     
+    
       let clue = 'Dica não encontrada';
       if (prefixData.length > 0 && prefixData[0].preview) {
         clue = prefixData[0].preview
-          .replace(/<\/?span.*?>/g, '')
-          .replace(/<\/?i.*?>/g, '')  
-          .split(';')[1]                // Divide a string pelo ";" e pega a segunda parte (após o primeiro ";")
+          .replace(/<\/?span.*?>/g, '') 
+          .replace(/<\/?i.*?>/g, '')   
+          .replace(/<\/?sup.*?>/g, '')   
+          .split(';')[1]                // Divide a string pelo ";" e pega a terceira parte (entre o primeiro e o segundo ";")
           .trim();                      // Remove espaços em branco extras
       }
   
       return { word, clue };
     } catch (error) {
       console.error('Erro ao obter palavra e dica:', error);
-      return { word: 'ERRO', clue: 'Erro ao buscar a palavra e a dica.' }; 
+      return { word: 'ERRO', clue: 'Erro ao buscar a palavra e a dica.' }; // Valor de fallback
     }
   }
   
