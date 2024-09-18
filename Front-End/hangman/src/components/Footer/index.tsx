db0f8aa3b18b6710f03fd19818a32a9747e8ae02
iangodinho <maxian.godinho@gmail.com>
2024-09-18 12:04:38 -0300

    Footer function created

--- Front-End/hangman/src/components/Footer/index.tsx
+++ Front-End/hangman/src/components/Footer/index.tsx
@@ -1,1 +1,24 @@
-import './Footer.css'
+import Button from '../Button';
+import ScoreBoard from '../ScoreBoard';
+import './Footer.css';
+
+interface FooterProps {
+  showNewWordButton: boolean;
+  initGame: () => void;
+  score: number;
+}
+
+const Footer: React.FC<FooterProps> = ({ showNewWordButton, initGame, score }) => {
+  return (
+    <div>
+      <div>
+        {showNewWordButton && <Button onClick={initGame}>Jogar novamente</Button>}
+      </div>
+      <div className="scoreBoard">
+        <ScoreBoard score={score} />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
