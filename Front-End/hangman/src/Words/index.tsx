3d6e258a11052d2bdadfbfea01fa3724d0db2b10
Gabriel Souza <gabrielmaestasouza@gmail.com>
2024-10-03 10:54:15 -0300

    Adapted receive API response

--- Front-End/hangman/src/Words/index.tsx
+++ Front-End/hangman/src/Words/index.tsx
@@ -2,21 +2,21 @@ import axios from "axios";
  
 // Interface correta para a resposta da API
 interface WordResponse {
-  wordName: string;
+  maskedWord: string;
   clue: string;
 }
  
 // Função que busca palavra e dica da API
 export async function getWordAndClue(): Promise<{ word: string; clue: string }> {
   try {
-    const response = await axios.get<WordResponse>('http://localhost:5155/api/hangman/getRandom', {
+    const response = await axios.get<WordResponse>('http://localhost:5155/api/hangman/NewGame', {
       headers: {
         'Accept': 'application/json',
       },
     });    
     // Renomeando os campos "text" para "word" e "tip" para "clue"
     console.log(response.data); 
-    return { word: response.data.wordName, clue: response.data.clue };
+    return { word: response.data.maskedWord, clue: response.data.clue };
   } catch (error) {
     console.error('Erro ao buscar a palavra:', error);
     throw new Error('Erro ao buscar a palavra e a dica.');
